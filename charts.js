a3a9389b4fa99e12ee120a8454ff07555b0154ef
unknown <scottwesleybean@gmail.com>
2022-09-09 17:26:02 -0600

    Updated files

--- charts.js
+++ charts.js
@@ -58,30 +58,51 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
-
+    var samples = data.samples;
+    console.log(samples);
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-
+    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+    
     //  5. Create a variable that holds the first sample in the array.
-
+    var result = resultArray[0];
+    console.log(result);
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-
+    var otu_ids = result.otu_ids;
+    console.log(otu_ids);
+    var otu_labels = result.otu_labels;
+    console.log(otu_labels);
+    var sample_values = result.sample_values;
+    console.log(sample_values);
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = 
+    var yticks = otu_ids.slice(0,10).map(id => "OTU " + id).reverse()
+    console.log(yticks);
+
+    // Top 10 otu_labels
+    var top10labels = otu_labels.slice(0,10).reverse();
+    console.log(top10labels);
+
+    // Top 10 sample_values
+    var top10samples = sample_values.slice(0,10).reverse();
+    console.log(top10samples);
 
     // 8. Create the trace for the bar chart. 
-    var barData = [
-      
-    ];
+    var barData = [{
+      x: top10samples,
+      y: yticks,
+      text: top10labels,
+      type: 'bar',
+      orientation: 'h'
+    }];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-     
+     title: "Top 10 Bacteria Cultures Found",
     };
     // 10. Use Plotly to plot the data with the layout. 
-    
+    Plotly.newPlot('bar', barData, barLayout);
   });
 }
